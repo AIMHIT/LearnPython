dee045ff0d8fb767a729955dbc05375fe44fe561
AIMHIT <YDRaznomastsev@stud.kantiana.ru>
2026-02-28 20:43:25 +0200

    Update main.py
    hw3 done

--- homework/2026_02_20/3_our_cache/main.py
+++ homework/2026_02_20/3_our_cache/main.py
@@ -1,5 +1,27 @@
 def cache(func):
-    pass
+    cached_results = {}
+    
+    def wrapper(*args):
+        norm_args = tuple(sorted(args))
 
-def my_sum(a, b):
-    return a + b
+        if norm_args in cached_results:
+            print("Беру из кеша")
+            return cached_results[norm_args]
+
+        result = func(*args)
+        cached_results[norm_args] = result
+        print("Вычисляю и кеширую...")
+        return result
+    return wrapper
+
+@cache
+def my_sum(*numbers):
+    return sum(numbers)
+
+def main():
+    print(my_sum(16, 26))
+    print(my_sum(26, 16))
+    print(my_sum(20, 16, 6))
+
+if __name__ == '__main__':
+    main()
