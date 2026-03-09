f313f3cf486de55add0cb06b83cd3c308cac4e91
AIMHIT <YDRaznomastsev@stud.kantiana.ru>
2026-03-09 14:04:29 +0200

    Update my_decorator.py
    hw2_2 done

--- homework/2026_02_27/retry/my_decorator.py
+++ homework/2026_02_27/retry/my_decorator.py
@@ -1,2 +1,20 @@
 def retry(count):
-    pass
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for attempt in range(count):
+                try:
+                    return func(*args, **kwargs)
+                except ValueError:
+                    print(f"Попытка {attempt + 1}: ValueError, пробуем снова...")
+                    continue
+                except OSError:
+                    print(f"{func.__name__} raise OSError exception.")
+                    continue
+                except Exception as e:
+                    print(f"Неожиданное исключение: {e}")
+                    raise
+
+            print(f"Все {count} попыток исчерпаны. Возвращаем None")
+            return None
+        return wrapper
+    return decorator
